6246c221435618076139f17ec98d06c0581d71cc
GAMA <abashia.nikoloza@gmail.com>
2023-02-04 23:24:07 +0400

    redberry

--- src/components/information/UserInfo.jsx
+++ src/components/information/UserInfo.jsx
@@ -11,22 +11,31 @@ const UserInfo = () => {
           <div className="page_point">1/3</div>
           <hr />
         </nav>
-        <div className="userInfo_left">
-          <form>
-            <div className="input_wrapper">
-              <div className="name_input">
+        <form>
+          <div className="userInfo_left">
+            <div className="inputs">
+              <div>
                 <label htmlFor="fname">სახელი</label>
-                <input type="text" name="" id="" />
+                <input type="text" />
                 <div className="error_message">min 2 aso</div>
               </div>
-              <div className="surname_input">
+              <div>
                 <label htmlFor="fname">გვარი</label>
-                <input type="text" name="" id="" />
+                <input type="text" />
                 <div className="error_message">min 2 aso</div>
               </div>
             </div>
-          </form>
-        </div>
+          </div>
+            <h1 className="add_photo">
+              პირადი ფოტოს ატვირთვა <button>ატვირთვა</button>
+            </h1>
+            <div className="about_yourself">
+             <h1>
+                ჩემ შესახებ (არასავალდებულოა)
+             </h1>
+             <input type="text" />
+            </div>
+        </form>
       </div>
       <div className="right">Niko</div>
     </div>
